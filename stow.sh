10c4209e3619d029fa1d837a610e665a9dc4bb52
ZanzyTHEbar <zacariahheim@gmail.com>
2025-05-27 14:21:14 +0100

    fix: migrate to systemd automount and away from autofs

--- stow.sh
+++ stow.sh
@@ -1,2 +1,20 @@
 stow -R -v config zsh
-sudo stow -R -t /etc -v etc
+#sudo stow -R -t /etc -v etc
+
+# Stow the systemd units to /etc/systemd/system/
+sudo stow -R  -t /etc/systemd/system -v systemd
+
+# Reload systemd to recognize new or updated units
+sudo systemctl daemon-reload
+
+# Enable and start each .automount unit
+for unit in systemd/*.automount; do
+    unit_name=$(basename "$unit")
+    sudo systemctl enable "$unit_name"
+    sudo systemctl start "$unit_name"
+done
+
+
+
+
+
