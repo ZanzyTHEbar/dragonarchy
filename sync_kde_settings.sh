6b3457e42780cd419ab7c2cba2eb3002c608731f
ZanzyTHEbar <zacariahheim@gmail.com>
2025-05-28 10:02:22 +0100

    feat: add nvim config

---
+++ sync_kde_settings.sh
@@ -0,0 +1,3 @@
+rsync -av --exclude='kscreen/' --exclude='pulse/' --exclude='NetworkManager/' ~/.config/ daofficialwizard@192.168.0.193:~/.config/
+rsync -av ~/.local/share/ daofficialwizard@192.168.0.193:~/.local/share/
+
