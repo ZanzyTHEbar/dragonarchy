978bb8b5832a4dfbd1782518d24c974f428c2a98
ZanzyTHEbar <zacariahheim@gmail.com>
2025-05-31 23:32:49 +0100

    feat: update systemd stow command with a dedicated script

---
+++ systemd/stow_systemd.sh
@@ -0,0 +1,12 @@
+sudo stow -R -t /etc/systemd/system -v systemd
+
+# Reload systemd to recognize new or updated units
+sudo systemctl daemon-reload
+
+# Enable and start each .automount unit
+for unit in systemd/*.automount; do
+    unit_name=$(basename "$unit")
+    echo "starting $unit_name"
+    sudo systemctl enable "$unit_name"
+    sudo systemctl start "$unit_name"
+done
