0a4002296b360e49e16dfe5a4ced35c3474be5f2
ZanzyTHEbar <zacariahheim@gmail.com>
2025-08-31 13:03:36 +0100

    fix: remove autojump zsh in favour of pure zoxide setup
    - move zsh functions to .config/zsh directory
    - fix duplicte pnpm zsh init logic

--- packages/zsh/.config/zsh/profile.zsh
+++ packages/zsh/.config/zsh/profile.zsh
@@ -1,20 +1,20 @@
 # ZSH Profile Configuration
 # Platform detection
 case "$(uname -s)" in
-    Darwin*) 
-        export PLATFORM="macos"
-        ;;
-    Linux*) 
-        export PLATFORM="linux"
-        # Detect specific Linux distributions
-        if [ -f /etc/os-release ]; then
-            . /etc/os-release
-            export LINUX_DISTRO="$ID"
-        fi
-        ;;
-    *)
-        export PLATFORM="unknown"
-        ;;
+Darwin*)
+    export PLATFORM="macos"
+    ;;
+Linux*)
+    export PLATFORM="linux"
+    # Detect specific Linux distributions
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        export LINUX_DISTRO="$ID"
+    fi
+    ;;
+*)
+    export PLATFORM="unknown"
+    ;;
 esac
 
 # Host detection
@@ -22,45 +22,45 @@ export HOSTNAME="$(hostname | cut -d. -f1)"
 
 # Load platform-specific configurations
 case "$PLATFORM" in
-    "macos")
-        # macOS specific exports
-        export HOMEBREW_PREFIX="/opt$HOMEbrew"
-        export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$PATH"
-        
-        # Load Homebrew environment if available
-        if [[ -f "$HOMEBREW_PREFIX/bin/brew" ]]; then
-            eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
-        fi
-        
-        # macOS specific aliases
-        alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
-        alias flushdns='sudo dscacheutil -flushcache && sudo killall -HUP mDNSResponder'
+"macos")
+    # macOS specific exports
+    export HOMEBREW_PREFIX="/opt$HOMEbrew"
+    export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$PATH"
+
+    # Load Homebrew environment if available
+    if [[ -f "$HOMEBREW_PREFIX/bin/brew" ]]; then
+        eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+    fi
+
+    # macOS specific aliases
+    alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
+    alias flushdns='sudo dscacheutil -flushcache && sudo killall -HUP mDNSResponder'
+    ;;
+
+"linux")
+    # Linux specific configurations
+    case "$LINUX_DISTRO" in
+    "arch" | "cachyos" | "manjaro")
+        # Arch-based distributions
+        alias pacu='sudo pacman -Syu'
+        alias pacs='pacman -Ss'
+        alias paci='sudo pacman -S'
+        alias pacr='sudo pacman -R'
+        alias pacclean='sudo pacman -Sc'
+        alias paruu='paru -Syu'
+        alias parus='paru -Ss'
+        alias parui='paru -S'
         ;;
-        
-    "linux")
-        # Linux specific configurations
-        case "$LINUX_DISTRO" in
-            "arch"|"cachyos"|"manjaro")
-                # Arch-based distributions
-                alias pacu='sudo pacman -Syu'
-                alias pacs='pacman -Ss'
-                alias paci='sudo pacman -S'
-                alias pacr='sudo pacman -R'
-                alias pacclean='sudo pacman -Sc'
-                alias paruu='paru -Syu'
-                alias parus='paru -Ss'
-                alias parui='paru -S'
-                ;;
-            "ubuntu"|"debian")
-                # Debian-based distributions
-                alias aptu='sudo apt update && sudo apt upgrade'
-                alias apts='apt search'
-                alias apti='sudo apt install'
-                alias aptr='sudo apt remove'
-                alias aptclean='sudo apt autoremove && sudo apt autoclean'
-                ;;
-        esac
+    "ubuntu" | "debian")
+        # Debian-based distributions
+        alias aptu='sudo apt update && sudo apt upgrade'
+        alias apts='apt search'
+        alias apti='sudo apt install'
+        alias aptr='sudo apt remove'
+        alias aptclean='sudo apt autoremove && sudo apt autoclean'
         ;;
+    esac
+    ;;
 esac
 
 export EDITOR=nvim
@@ -70,13 +70,13 @@ export FCEDIT=nvim
 export TERMINAL=kitty
 export BROWSER=vivaldi
 if [[ -x "$(command -v bat)" ]]; then
-	export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-	export PAGER=bat
+    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+    export PAGER=bat
 fi
 export LIBVIRT_DEFAULT_URI="qemu:///system"
 export LS_COLORS=$(echo $LS_COLORS | sed "s/ow=34/ow=37;40/")
 # Development environment setup
-if command -v pyenv &> /dev/null; then
+if command -v pyenv &>/dev/null; then
     export PYENV_ROOT="$HOME/.pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
@@ -95,13 +95,13 @@ if [[ -f "$HOME/.cargo/env" ]]; then
 fi
 
 # Go environment
-if command -v go &> /dev/null; then
+if command -v go &>/dev/null; then
     export GOPATH="$HOME/go"
     export PATH="$GOPATH/bin:$PATH"
 fi
 
-# Ruby environment  
-if command -v rbenv &> /dev/null; then
+# Ruby environment
+if command -v rbenv &>/dev/null; then
     eval "$(rbenv init -)"
 fi
 
@@ -112,8 +112,7 @@ fi
 
 # Path manipulation functions
 pathappend() {
-    for ARG in "$@"
-    do
+    for ARG in "$@"; do
         if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
             PATH="${PATH:+"$PATH:"}$ARG"
         fi
@@ -121,8 +120,7 @@ pathappend() {
 }
 
 pathprepend() {
-    for ARG in "$@"
-    do
+    for ARG in "$@"; do
         if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
             PATH="$ARG${PATH:+":$PATH"}"
         fi
@@ -132,7 +130,7 @@ pathprepend() {
 # Apply PATH configuration using path functions
 pathprepend "$HOME/bin" "$HOME/sbin" "$HOME/.local/bin" "$HOME/local/bin" "$HOME/.bin" "$HOME/.local/share/dragon"
 pathappend "/usr/local/bin" "/usr/local/go/bin"
-pathappend 
+pathappend
 
 # Development environment paths
 if [[ -n "$GOPATH" ]]; then
@@ -144,9 +142,8 @@ pathappend "/mnt/common/bin"
 # PNPM setup
 export PNPM_HOME="$HOME/.local/share/pnpm"
 case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
+*":$PNPM_HOME:"*) ;;
+*) export PATH="$PNPM_HOME:$PATH" ;;
 esac
-
 
 [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
