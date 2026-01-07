0ee40a4afdb30a4e0d0ac21ede3bd41911adf3e4
ZanzyTHEbar <zacariahheim@gmail.com>
2026-01-07 21:04:27 +0000

    feat: implement NFS tooling with robust mount management and shared configurations

---
+++ hosts/dragon/dotfiles/.config/zsh/hosts/dragon.zsh
@@ -0,0 +1,5 @@
+# Host opt-in: shared NFS-safe tooling (/mnt/common)
+
+if [[ -r "$HOME/.config/zsh/hosts/shared/nfs-common.zsh" ]]; then
+    source "$HOME/.config/zsh/hosts/shared/nfs-common.zsh"
+fi
