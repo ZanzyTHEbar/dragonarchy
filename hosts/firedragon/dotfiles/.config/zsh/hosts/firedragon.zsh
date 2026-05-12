0a0241125c713db0b0747258a8c6a2404629be35
ZanzyTHEbar <zacariahheim@gmail.com>
2026-05-12 13:04:30 +0100

    chore: stability work for opencode and firedragon host

--- hosts/firedragon/dotfiles/.config/zsh/hosts/firedragon.zsh
+++ hosts/firedragon/dotfiles/.config/zsh/hosts/firedragon.zsh
@@ -5,3 +5,7 @@ if [[ -r "$HOME/.config/zsh/hosts/shared/nfs-common.zsh" ]]; then
 fi
 
 export MEMORY_BANK_ROOT="/mnt/dragonnet/.memory/memory-bank/"
+
+export PATH=/home/daofficialwizard/bin:$PATH
+
+[[ -e "/home/daofficialwizard/lib/oracle-cli/lib/python3.14/site-packages/oci_cli/bin/oci_autocomplete.sh" ]] && source "/home/daofficialwizard/lib/oracle-cli/lib/python3.14/site-packages/oci_cli/bin/oci_autocomplete.sh"
